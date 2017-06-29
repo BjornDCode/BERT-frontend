d2750ab3a9ed2f687eed422960595870fc701cc4
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-06-29 18:32:01 +0200

    Sass variables and mixins

---
+++ src/components/common/RouteButton.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class RouteButton extends Component {
+
+    render() {
+
+        const { size, color, redirectTo, text } = this.props;
+
+        return(
+            <button className={"button " + size + " " + color}><Link to={redirectTo}>{text}</Link></button>
+        );
+    }
+}
+
+RouteButton.propTypes = {
+    size: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    redirectTo: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+};
+
+export default RouteButton;
--- src/components/index/IndexPage.js
+++ src/components/index/IndexPage.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import RouteButton from '../common/RouteButton';
 
 class IndexPage extends Component {
 
     render() {
         return(
-            <div className="app">
-                <h1>IndexPage</h1>
+            <div className="index-page">
+                <div className="intro">
+                    <h1>Easily generate Bipolar Emotional Response Tests (BERTs) with this tool!</h1>
+                </div>
+                <div className="cta">
+                    <RouteButton size="big" color="brand" redirectTo="/signup" text="Sign Up" />
+                    <RouteButton size="big" color="white" redirectTo="/signin" text="Sign In" />
+                </div>
             </div>
         );
     }
