932a5b56f7f9f9b64b2e6947327659cf4bb11946
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-07-02 10:43:51 +0200

    Temporarily added api url in config file

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,2 @@
-export const BERT_API_URL = process.env.REACT_APP_BERT_API_URL;
+// export const BERT_API_URL = process.env.REACT_APP_BERT_API_URL;
+export const BERT_API_URL = 'http://api.bertgenerator.com';
